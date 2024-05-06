3dd02d4ab4fc5590d734ddc858390e5b3478bff2
mask-shakill <mask-shakill>
2024-05-06 22:40:30 +0600

    update banner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -17,7 +17,7 @@ const Banner = () => {
     <div className="flex pl-[6.187rem] pr-[4.81rem] text-white relative">
       <div className="w-[542px] h-[150px] pt-[200px]">
         <h1 className="text-[45px] font-semibold">
-          Transform your <br /> space with elegance.
+          Transform your <br /> space with elegance
         </h1>
         <div className="pt-[83px] flex gap-x-[14px]">
           <button className="bg-yellow-400 rounded-[30px] text-black w-[160px] h-[50px] ">
